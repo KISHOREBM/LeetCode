8c30280a23fb33d182860a41540eaf24c40757e3
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2024-11-06 17:50:29 +0530

    Time: 98 ms (5.71%) | Memory: 45.6 MB (5.66%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -5,39 +5,12 @@ class Solution {
         while(a>0)
         {
             int rem=a%2;
+            if(rem!=0)
             ret+=rem;
             a=a/2;
         }
         return  ret;
-    }
-    public boolean canswap(int a, int b)
-    {
-        String bita=bitform(a),bitb=bitform(b);
-        int counta=0,countb=0,i=0,j=0;
-        while(i<bita.length() && j<bitb.length())
-        {
-            if(bita.charAt(i)=='1')
-            counta+=1;
-            if(bitb.charAt(j)=='1')
-            countb+=1;
-            i++;j++;
-        }
-        while(i<bita.length()){
-        if(bita.charAt(i)=='1')
-        {
-            counta++;
-        }
-        i++;}
-        while(j<bitb.length()){
-        if(bitb.charAt(j)=='1')
-        {
-            countb++;
-        }
-        j++;}
-        if(counta==countb)
-        return true;
-        return false;
-    }   
+    } 
     public boolean canSortArray(int[] nums) {
         for(int i=0;i<nums.length;i++)
         {
@@ -45,7 +18,7 @@ class Solution {
             {
                 if(nums[j]>nums[j+1])
                 {
-                    if(canswap(nums[j],nums[j+1]))
+                    if (bitform(nums[j]).length() == bitform(nums[j + 1]).length())
                     {
                         int temp=nums[j];
                         nums[j]=nums[j+1];
