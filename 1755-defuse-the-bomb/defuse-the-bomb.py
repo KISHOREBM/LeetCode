4aeb50f8a82383b8c14041143c73d92ec49cf97c
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2024-11-18 12:52:32 +0530

    Time: 17 ms (8.11%) | Memory: 11.6 MB (31.63%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    @staticmethod
+    def minus(code,n):
+        i=0
+        ret=[0]*len(code)
+        while i<len(code):
+            for j in range(n):
+                ret[i]+=code[(len(code)-j-1+i)%len(code)]
+            i+=1
+        return ret
+    @staticmethod
+    def plusd(code,n):
+        i=0
+        ret=[0]*len(code)
+        while i<len(code):
+            for j in range(n):
+                ret[i]+=code[(i+j+1)%len(code)]
+            i+=1
+        return ret
+    def decrypt(self, code, k):
+        ret=[0]*len(code)
+        i=0
+        if k<0:
+            n=k*-1
+            return Solution.minus(code,n)
+        elif k==0:
+            return [0]*len(code)
+        else:
+            return Solution.plusd(code,k)
+        
+        
