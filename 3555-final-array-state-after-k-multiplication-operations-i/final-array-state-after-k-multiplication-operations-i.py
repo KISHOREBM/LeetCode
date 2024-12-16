15031f7d1625efc87c61696cce8c5ef1427bf56f
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2024-12-16 17:48:15 +0530

    Time: 3 ms (49.65%) | Memory: 12.2 MB (7.74%) - LeetSync

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def getFinalState(self, nums, k, multiplier):
+        """
+        :type nums: List[int]
+        :type k: int
+        :type multiplier: int
+        :rtype: List[int]
+        """
+        for i in range(k):
+            small=nums[0]
+            mini=0
+            for j in range(1,len(nums)):
+                if nums[j] < small:
+                    small=nums[j]
+                    mini=j
+            nums[mini]=small*multiplier
+        return nums
+        
