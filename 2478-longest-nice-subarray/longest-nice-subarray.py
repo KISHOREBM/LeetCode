2ae8d67ff4b6f1b9411d61301351556b58622021
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2025-03-18 22:07:29 +0530

    Time: 62 ms (100.00%) | Memory: 21.7 MB (76.00%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.py
+++ 2478-longest-nice-subarray/longest-nice-subarray.py
@@ -1,16 +1,17 @@
 class Solution(object):
     def longestNiceSubarray(self, nums):
-        used = 0  # Tracks OR of the current subarray
-        left = 0  # Left pointer of the window
-        max_length = 0  # Stores the longest nice subarray length
+        count=1
 
-        for right in range(len(nums)):
-            # If adding nums[right] causes conflict, shrink window from the left
-            while (used & nums[right]) != 0:
-                used ^= nums[left]  # Remove nums[left] from OR tracker
-                left += 1  # Move left pointer
-            
-            used |= nums[right]  # Add nums[right] to OR tracker
-            max_length = max(max_length, right - left + 1)  # Update max length
+        for i in range(len(nums)):
+            ma=1
+            j=i+1
+            used=nums[i]
+            while j<len(nums) and used&nums[j]==0:
+                ma+=1
+                used|=nums[j]
+                j+=1
+            if ma>count:
+                count=ma
+        return count
 
-        return max_length
+        
