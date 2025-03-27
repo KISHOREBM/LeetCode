213ff37e4bccad1ec14e5f71a0d9007d5e069864
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2025-03-27 11:50:50 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (98.89%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -2,26 +2,26 @@ class Solution(object):
     def plusOne(self, digits):
         j=len(digits)-1
         carry=0
-        # while j>=0:
-        #     if digits[j]+1>=10:
-        #         carry=1
-        #         digits[j]=0
-        #         j-=1
-        #     else:
-        #         digits[j]+=1
-        #         carry=0
-        #         break
-        # print(digits)
-        # if carry==1:
-        #     digits.insert(0,carry)
-        # return digits
         while j>=0:
-            if digits[j]<9:
+            if digits[j]+1>=10:
+                carry=1
+                digits[j]=0
+                j-=1
+            else:
                 digits[j]+=1
-                return digits
-            digits[j]=0
-            j-=1
-        return [1]+digits
+                carry=0
+                break
+        # print(digits)
+        if carry==1:
+            digits.insert(0,carry)
+        return digits
+        # while j>=0:
+        #     if digits[j]<9:
+        #         digits[j]+=1
+        #         return digits
+        #     digits[j]=0
+        #     j-=1
+        # return [1]+digits
 
 
 
