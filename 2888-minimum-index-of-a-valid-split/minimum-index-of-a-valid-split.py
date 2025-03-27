92a4b48ef1548725adccb0642a36596563644154
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2025-03-27 23:47:51 +0530

    Time: 121 ms (71.43%) | Memory: 25 MB (66.67%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -3,11 +3,12 @@ class Solution(object):
     def minimumIndex(self, nums):
         value,count=Counter(nums).most_common(1)[0]
         prec=0
-        for i in range(len(nums)):
+        n=len(nums)
+        for i in range(n):
             if nums[i]==value:
                 prec+=1
                 count-=1
-                if (i+1-prec)<prec and ((len(nums)-i-1)-count)<count :
+                if (i+1-prec)<prec and (n-i-1-count)<count :
                     return i
                 # print(i+1-prec,(len(nums)-i-1)-count,prec,count)
         return -1
