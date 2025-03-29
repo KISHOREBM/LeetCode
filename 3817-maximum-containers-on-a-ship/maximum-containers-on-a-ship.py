0432a6d1f980a93b356427928142d02bfc93dc19
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2025-03-29 09:39:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (22.01%) - LeetSync

--- 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.py
+++ 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def maxContainers(self, n, w, maxWeight):
-        n=n*n
-        while(n>0):
-            if n*w <=maxWeight:
-                return n
-            n-=1
-        return 0
+        # n=n*n
+        # while(n>0):
+        #     if n*w <=maxWeight:
+        #         return n
+        #     n-=1
+        # return 0
+        return min(n*n,maxWeight/w)
         
