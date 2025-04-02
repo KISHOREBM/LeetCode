88c7455e76d140dd2225a6b4c10c251b456ae819
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2025-04-02 07:15:32 +0530

    Time: 9 ms (83.33%) | Memory: 12.4 MB (83.33%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def maximumTripletValue(self, nums):
         mv=0
-        k=0
         ls=0
         for i in range(len(nums)-1):
             for j in range(i+1,len(nums)):
@@ -12,7 +11,5 @@ class Solution(object):
                     for k in nums[j+1:]:
                         if mv*k>ls:
                             ls=mv*k
-        
-        
         return ls
         
