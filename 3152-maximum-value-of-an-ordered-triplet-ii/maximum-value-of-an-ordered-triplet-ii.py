388b58143764bc9f3f09270d9c04f3cf1cefdbc3
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2025-04-04 01:28:05 +0530

    Time: 7506 ms (8.33%) | Memory: 21.4 MB (79.17%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.py
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.py
@@ -1,24 +1,20 @@
 class Solution(object):
     def maximumTripletValue(self, nums):
-        n = len(nums)
-        if n < 3:
-            return 0
-        
-        max_right = [0] * n  # Stores max value to the right of each index
-        max_right[-1] = nums[-1]
-        
-        # Compute suffix max in one pass
-        for i in range(n-2, -1, -1):
-            max_right[i] = max(max_right[i+1], nums[i])
-        
-        max_left = nums[0]  # Keeps track of max value on the left
-        max_triplet = 0
-        
-        # Iterate through the middle element
-        for j in range(1, n-1):
-            diff = max_left - nums[j]
-            if diff > 0:
-                max_triplet = max(max_triplet, diff * max_right[j+1])
-            max_left = max(max_left, nums[j])  # Update left max
-        
-        return max_triplet
+        ma=0
+        mi=0
+        ret=0
+        marr=[nums[-1]]
+        for i in nums[-1::-1]:
+            if i > marr[0]:
+                marr.insert(0,i)
+            else:
+                marr.insert(0,marr[0])
+        print(marr)
+        for j,i in enumerate(nums):
+            ret=max(ret,(ma-mi)*marr[j])
+            if i>ma:
+                ma=i
+                mi=i
+            if i<mi:
+                mi=i
+        return ret
