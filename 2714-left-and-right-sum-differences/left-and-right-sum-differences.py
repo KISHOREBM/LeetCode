87b2f648db74ffa30d5d3269b6fc005e03b55227
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2025-04-05 12:49:20 +0530

    Time: 7 ms (46.44%) | Memory: 12.7 MB (45.61%) - LeetSync

---
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def leftRightDifference(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        rightsum=[0]
+        s=sum(nums[1:])
+        # print(s)
+        count=0
+        for i in nums[1:]:
+            rightsum.insert(count,s)
+            s=s-i
+            count+=1
+        s=0
+        leftsum=[0]
+        for i in nums[:-1]:
+            s=s+i
+            leftsum.append(s)
+        # print(leftsum,rightsum)
+        return [abs(leftsum[i]-rightsum[i]) for i in range(len(leftsum)) ]
+
+        
