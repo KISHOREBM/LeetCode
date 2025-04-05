7bec5499a867bebbcf9020230e0ee538927d450e
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2025-04-05 12:35:14 +0530

    Time: 2 ms (70.35%) | Memory: 23.5 MB (0.25%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.py
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -5,22 +5,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        count=0
-        n=len(nums)
-        nums=np.array(nums)
-        while count<n:
-            min1=np.min(nums[count:])
-            # print(np.argmin(nums[count:]))
-            nums=np.delete(nums,np.argmin(nums[count:])+count)
-            min2=np.min(nums[count:])
-            # print(np.argmin(nums[count:]))
-            nums=np.delete(nums,np.argmin(nums[count:])+count)
-            # print(nums[count:],count)
-            nums=np.insert(nums,count,min2)
-            count+=1
-            # print(nums,count)
-            nums=np.insert(nums,count,min1)
-            count+=1
-            # print(nums,count)
-        return list(nums)
-        
+        nums.sort()
+        for i in range(0,len(nums),2):
+            nums[i],nums[i+1]=nums[i+1],nums[i]
+        return nums
