5b375bbb9601ae07ffa757db6073f65bea39aa8a
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2025-04-22 01:01:00 +0530

    Time: 1432 ms (5.03%) | Memory: 15.3 MB (69.37%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+        coins={
+            5:0,10:0,20:0
+        }
+        values=[20,10,5]
+        for i in bills:
+            if i!=5:
+                amount=i-5
+                j=0
+                while j<len(values) and   amount>0:
+                    x=(amount//values[j])
+                    if coins[values[j]]-x>=0:
+                        coins[values[j]]-=x
+                        # print(coins,amount//values[j])
+                        amount=amount%values[j]
+                    j+=1
+                # print(amount,'a')
+                if amount>0:
+                    return False
+            coins[i]+=1
+            print(coins,'co')
+        return True
+            
+
+        
