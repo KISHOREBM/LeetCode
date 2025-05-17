271c4b02ee544dd09108d8500edf62e4d4fd3db9
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2025-05-17 18:58:58 +0530

    Time: 4 ms (97.85%) | Memory: 13.5 MB (73.95%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -5,7 +5,6 @@ class Solution(object):
         for i in set(s):
             d[i]=s.count(i)
         s_values=[a for _,a in sorted(zip(list(d.values()),list(d.keys())))[::-1]]
-        # print(sorted(zip(value,key)))
         for i in s_values:
             ret+=i*s.count(i)
         return ret
