b8226a7d24311d5bf5234e27807d094002075da9
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2025-06-18 15:22:55 +0530

    Time: 95 ms (100.00%) | Memory: 29.6 MB (91.67%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -5,15 +5,11 @@ class Solution(object):
         left=0
         right=2
         while(right<len(nums)):
-            valid=0
             if nums[right]-nums[left]<=k:
                 ret.append(nums[left:right+1])
                 left+=3
                 right+=3
             else:
-                # print(left,right,ret)
                 return []
-            # if len(ret)==len(nums)//3:
-            #     return ret
         return ret
         
