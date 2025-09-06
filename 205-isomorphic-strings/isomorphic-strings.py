3b2f14a790826cd407873c8839f2da152273cfbf
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2025-09-06 23:01:19 +0530

    Time: 8 ms (70.02%) | Memory: 15.3 MB (10.15%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        if len(s) != len(t):
+            return False
+        
+        map_s, map_t = {}, {}
+        
+        for i, j in zip(s, t):
+            if (i in map_s and map_s[i] != j) or (j in map_t and map_t[j] != i):
+                return False
+            map_s[i] = j
+            map_t[j] = i
+        
+        return True
